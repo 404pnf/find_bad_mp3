6df11ba08c97aae0471fbb3bd7d76adaa4da17ef
404pnf <404pnf@exmaple.org>
2013-01-28 14:54:08 +0800

    do not know how to write test for the script!!

--- find_bad_mp3.rb
+++ find_bad_mp3.rb
@@ -1,18 +1,23 @@
+#! /usr/bin/env ruby
 require 'crack'
 require 'pp'
 
-inputfile = File.expand_path ARGV[0]
+(puts "Usage: #{$0} input.xml output.txt"; exit;) unless ARGV.size == 2
+DEBUG = nil
+
+INPUTFILE = File.expand_path ARGV[0]
 outputfile = File.expand_path ARGV[1]
 
-(puts "Usage: #{0} input.xml output.txt"; exit;) unless ARGV.size == 2
+def find_bad_mp3 input
+  input = INPUTFILE
+  hash =  Crack::XML.parse(File.read input)
+  array_of_mp3 = hash['Mediainfo']['File']
+  hash_of_bad_mp3 = array_of_mp3.reject { |i| i['track'].is_a? Array}
+  mp3_filenames = hash_of_bad_mp3.collect { |i| i['track']['CompleteName'] }
+end
 
-hash =  Crack::XML.parse(File.read inputfile)
-
-array_of_mp3 = hash['Mediainfo']['File']
-
-hash_of_bad_mp3 = array_of_mp3.reject { |i| i['track'].is_a? Array}
-
-mp3_filenames = hash_of_bad_mp3.collect { |i| i['track']['CompleteName'] }
+mp3_filenames =  find_bad_mp3 INPUTFILE
+p mp3_filenames if DEBUG # for test script
 
 File.write(outputfile, mp3_filenames.join("\n"))
 
--- test/test_find_bad_mp3.rb
+++ test/test_find_bad_mp3.rb
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
 require 'test/unit'
 require_relative '../find_bad_mp3.rb'
 
-class TestFindBadMp3 < Test::TestUnit::TestCase
-
-  INPUT = './test.xml'
-  OUTPUT = './test.result.txt'
-  OUTPUT_STRING = "C:\\Users\\tian\\Desktop\\itest-专八不正常音频\\2011081616521502.mp3"
+class TestFindBadMp3 < Test::Unit::TestCase
+  DEBUG = true
+  #EXPECTED = 'C:\Users\tian\Desktop\itest-专八不正常音频\2011081616521502.mp3'
   def test_find_bad_mp3
-    assert_equal  OUTPUT_STRING, (find_bad_mp3 INPUT, OUTPUT)
+    assert_equal EXPECTED, find_bad_mp3('test.xml', 'rst.txt')
   end
-  
-end
-  
-  
+end 
